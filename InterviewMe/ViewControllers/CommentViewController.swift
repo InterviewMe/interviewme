42468141a723a32f0b6973cbec009cea23550779
sks3 <saketsingh2018@gmail.com>
2018-04-22 23:35:19 -0500

    Update license

--- InterviewMe/ViewControllers/CommentViewController.swift
+++ InterviewMe/ViewControllers/CommentViewController.swift
@@ -1,55 +1,76 @@
-//
-//  CommentViewController.swift
-//  InterviewMe
-//
-//  Created by Tianyu Liang on 4/22/18.
-//  Copyright © 2018 Somi Singh. All rights reserved.
-//
+/// Copyright (c) 2018 InterviewMe: Tianyu Liang, Somi Singh, Henry Vuong
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import UIKit
 import Parse
 
 class CommentViewController: UIViewController {
-
-    @IBOutlet weak var comment: UITextView!
-    var post: Post?
+  
+  @IBOutlet weak var comment: UITextView!
+  var post: Post?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func comment(_ sender: Any) {
-        Comment.comment(withPostText: comment.text, withPost: post) { (success, error) in
-            if(success){
-                print("posted")
-                self.dismiss(animated: true, completion: nil)
-                
-            }else{
-                print("failed")
-            }
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func dismiss(_ sender: Any) {
+    // Do any additional setup after loading the view.
+  }
+  @IBAction func comment(_ sender: Any) {
+    Comment.comment(withPostText: comment.text, withPost: post) { (success, error) in
+      if(success){
+        print("posted")
         self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-            
         
+      }else{
+        print("failed")
+      }
     }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func dismiss(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     
-
+    
+  }
+  
+  
 }
+
